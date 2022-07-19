3c88809bf358b5183ce56411ce40aeb671435bb5
Himmat <himmatsawant0067@gmail.com>
2022-07-19 23:28:45 +0530

    Login Functionality

--- app/src/main/java/com/himmat/notes/LoginActivity.kt
+++ app/src/main/java/com/himmat/notes/LoginActivity.kt
@@ -3,14 +3,27 @@ package com.himmat.notes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelStores
 import com.himmat.notes.databinding.ActivityLoginBinding
+import com.himmat.notes.viewmodels.LoginViewModel
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var loginViewModel: LoginViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val bindingLogin: ActivityLoginBinding = DataBindingUtil.setContentView(this,R.layout.activity_login);
+        val bindingLogin: ActivityLoginBinding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+
+        loginViewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+        // send loginViewModel to xml
+        bindingLogin.loginViewModel = loginViewModel
+
+        bindingLogin.lifecycleOwner = this
 
 
     }
--- app/src/main/java/com/himmat/notes/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/himmat/notes/viewmodels/LoginViewModel.kt
@@ -1,4 +1,21 @@
 package com.himmat.notes.viewmodels
 
-class LoginViewModel {
+import android.util.Log
+import android.widget.Toast
+import androidx.databinding.Bindable
+import androidx.databinding.Observable
+import androidx.databinding.ObservableField
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel(): ViewModel() {
+
+    val TAG: String = "LoginViewModel"
+
+    var inputEmailOrPhone = ObservableField("")
+    var inputPassword = ObservableField("")
+
+    fun onLoginBtnClick(){
+        Log.d(TAG, "onLoginBtnClick: Login Button Clicked ${inputEmailOrPhone.get().toString()} and ${inputPassword.get().toString()}")
+    }
 }
